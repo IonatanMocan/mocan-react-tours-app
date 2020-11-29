28a0daf803f57379acdb90a24e6f8cce56a7e8a5
= <=>
2020-11-29 20:45:44 +0200

    initial commit

---
+++ build/precache-manifest.99e5ffb99c95a8dde375221d37d2f4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40f289088ac41f871020403e0e88521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f68935a06ccb0260fc",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f68935a06ccb0260fc",
+    "url": "/static/js/main.1da64f76.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
